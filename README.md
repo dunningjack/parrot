757cb5ed9d9d5431ef9e37085b1215b8f89e5cc2
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-10-20 14:09:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # parrot
 A discord bot that parrots what you say
+
+Users can summon the bot and provide a message, which the bot will repeat _n_ times
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.startswith("!p"):
+        parrot(message)
+        return
+
+def parrot(message):
+    command = message.split(" ")
+    if command[0].isdigit:
+        iterations = command[0]
+        for i in range(1, (len(command))-1):
+            separator = ', '
+            separator.join(command)
+    elif:
+        print("Error: No iterations entered")
