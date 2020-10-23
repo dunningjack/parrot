63dc315492201c5bf002cf50a2150d9165cda98b
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-10-23 15:35:46 +0100

    Initial commit

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
@@ -18,12 +20,14 @@ async def on_message(message):
         parrot(message)
         return
 
+
 def parrot(message):
     command = message.split(" ")
     if command[0].isdigit:
         iterations = command[0]
-        for i in range(1, (len(command))-1):
+        for i in range(1, (len(command)) - 1):
             separator = ', '
             separator.join(command)
-    elif:
-        print("Error: No iterations entered")
+
+
+client.run(TOKEN)
