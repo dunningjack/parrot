ce0771418aa139ced7d240b8a362276e9be704e0
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-10-23 15:57:52 +0100

    Basic

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!p')
 
 
 @client.event
@@ -14,20 +15,26 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 
-@client.event
+@client.command()
 async def on_message(message):
-    if message.startswith("!p"):
+    if '!p' in message.content:
         parrot(message)
-        return
+    return
 
 
-def parrot(message):
+@client.event
+async def parrot(message):
     command = message.split(" ")
-    if command[0].isdigit:
-        iterations = command[0]
-        for i in range(1, (len(command)) - 1):
+    if command[1].isdigit:
+        iterations = command[1]
+        for i in range(1, (len(command))):
             separator = ', '
-            separator.join(command)
+            statement = separator.join(command)
+
+        for j in range(1, iterations):
+            await client.say(statement)
+
+    await client.process_commands(message)
 
 
 client.run(TOKEN)
