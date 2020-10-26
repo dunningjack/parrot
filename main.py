9118b3e199cf4751dbfd1190fcda03f20273283a
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-10-26 23:16:39 +0000

    Minor changes to test message responses

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -17,9 +16,8 @@ async def on_ready():
 
 @client.command()
 async def on_message(message):
-    if '!p' in message.content:
-        parrot(message)
-    return
+    if message.content.startswith("!p"):
+        await parrot(message)
 
 
 @client.event
@@ -27,12 +25,12 @@ async def parrot(message):
     command = message.split(" ")
     if command[1].isdigit:
         iterations = command[1]
-        for i in range(1, (len(command))):
+        for i in range(2, (len(command))):
             separator = ', '
             statement = separator.join(command)
 
         for j in range(1, iterations):
-            await client.say(statement)
+            await client.send_message(message.channel, statement)
 
     await client.process_commands(message)
 
